d5b25bb2b8704272de9fff5702ed7c42f442d92d
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2018-03-22 08:54:09 +0100

    Modificata query INSERT, ora è strutturata correttamente

--- VBPostgreSQL_Connection/Form1.Designer.vb
+++ VBPostgreSQL_Connection/Form1.Designer.vb
@@ -35,21 +35,27 @@ Partial Class Form1
         Me.txtGL_AppObjUID = New System.Windows.Forms.TextBox()
         Me.txtGL_AziendaID = New System.Windows.Forms.TextBox()
         Me.btnSQLServer_Connection = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSQLServerGetAllData = New System.Windows.Forms.Button()
+        Me.lblStatoSQLServer = New System.Windows.Forms.Label()
         CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnCloseConnection
         '
-        Me.btnCloseConnection.Location = New System.Drawing.Point(249, 14)
+        Me.btnCloseConnection.Location = New System.Drawing.Point(250, 24)
         Me.btnCloseConnection.Name = "btnCloseConnection"
-        Me.btnCloseConnection.Size = New System.Drawing.Size(108, 23)
+        Me.btnCloseConnection.Size = New System.Drawing.Size(86, 23)
         Me.btnCloseConnection.TabIndex = 18
         Me.btnCloseConnection.Text = "Close connection"
         Me.btnCloseConnection.UseVisualStyleBackColor = True
         '
         'btnSelect_Classificazione
         '
-        Me.btnSelect_Classificazione.Location = New System.Drawing.Point(173, 43)
+        Me.btnSelect_Classificazione.Location = New System.Drawing.Point(166, 53)
         Me.btnSelect_Classificazione.Name = "btnSelect_Classificazione"
         Me.btnSelect_Classificazione.Size = New System.Drawing.Size(165, 23)
         Me.btnSelect_Classificazione.TabIndex = 17
@@ -58,9 +64,9 @@ Partial Class Form1
         '
         'btnGetTables
         '
-        Me.btnGetTables.Location = New System.Drawing.Point(23, 98)
+        Me.btnGetTables.Location = New System.Drawing.Point(16, 108)
         Me.btnGetTables.Name = "btnGetTables"
-        Me.btnGetTables.Size = New System.Drawing.Size(447, 23)
+        Me.btnGetTables.Size = New System.Drawing.Size(425, 23)
         Me.btnGetTables.TabIndex = 16
         Me.btnGetTables.Text = "Get DB tables"
         Me.btnGetTables.UseVisualStyleBackColor = True
@@ -68,16 +74,16 @@ Partial Class Form1
         'dgvData
         '
         Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvData.Location = New System.Drawing.Point(12, 207)
+        Me.dgvData.Location = New System.Drawing.Point(12, 288)
         Me.dgvData.Name = "dgvData"
-        Me.dgvData.Size = New System.Drawing.Size(483, 276)
+        Me.dgvData.Size = New System.Drawing.Size(483, 195)
         Me.dgvData.TabIndex = 15
         '
         'btnSelectContati
         '
-        Me.btnSelectContati.Location = New System.Drawing.Point(344, 43)
+        Me.btnSelectContati.Location = New System.Drawing.Point(337, 53)
         Me.btnSelectContati.Name = "btnSelectContati"
-        Me.btnSelectContati.Size = New System.Drawing.Size(151, 23)
+        Me.btnSelectContati.Size = New System.Drawing.Size(129, 23)
         Me.btnSelectContati.TabIndex = 14
         Me.btnSelectContati.Text = "Select * from Contatti"
         Me.btnSelectContati.UseVisualStyleBackColor = True
@@ -86,7 +92,7 @@ Partial Class Form1
         '
         Me.lblStato.AutoSize = True
         Me.lblStato.Cursor = System.Windows.Forms.Cursors.Arrow
-        Me.lblStato.Location = New System.Drawing.Point(363, 19)
+        Me.lblStato.Location = New System.Drawing.Point(356, 29)
         Me.lblStato.Name = "lblStato"
         Me.lblStato.Size = New System.Drawing.Size(107, 13)
         Me.lblStato.TabIndex = 13
@@ -94,16 +100,16 @@ Partial Class Form1
         '
         'btnInsert_GL_Lavori
         '
-        Me.btnInsert_GL_Lavori.Location = New System.Drawing.Point(12, 70)
+        Me.btnInsert_GL_Lavori.Location = New System.Drawing.Point(5, 80)
         Me.btnInsert_GL_Lavori.Name = "btnInsert_GL_Lavori"
-        Me.btnInsert_GL_Lavori.Size = New System.Drawing.Size(196, 23)
+        Me.btnInsert_GL_Lavori.Size = New System.Drawing.Size(174, 23)
         Me.btnInsert_GL_Lavori.TabIndex = 12
         Me.btnInsert_GL_Lavori.Text = "Insert into GL_Lavori"
         Me.btnInsert_GL_Lavori.UseVisualStyleBackColor = True
         '
         'btnSelect
         '
-        Me.btnSelect.Location = New System.Drawing.Point(12, 43)
+        Me.btnSelect.Location = New System.Drawing.Point(5, 53)
         Me.btnSelect.Name = "btnSelect"
         Me.btnSelect.Size = New System.Drawing.Size(155, 23)
         Me.btnSelect.TabIndex = 11
@@ -112,66 +118,111 @@ Partial Class Form1
         '
         'btnConnect
         '
-        Me.btnConnect.Location = New System.Drawing.Point(12, 14)
+        Me.btnConnect.Location = New System.Drawing.Point(5, 24)
         Me.btnConnect.Name = "btnConnect"
-        Me.btnConnect.Size = New System.Drawing.Size(231, 23)
+        Me.btnConnect.Size = New System.Drawing.Size(239, 23)
         Me.btnConnect.TabIndex = 10
         Me.btnConnect.Text = "Connect to DB"
         Me.btnConnect.UseVisualStyleBackColor = True
         '
         'txtGL_IDLavoro
         '
-        Me.txtGL_IDLavoro.Location = New System.Drawing.Point(218, 72)
+        Me.txtGL_IDLavoro.Location = New System.Drawing.Point(185, 82)
         Me.txtGL_IDLavoro.Name = "txtGL_IDLavoro"
-        Me.txtGL_IDLavoro.Size = New System.Drawing.Size(91, 20)
+        Me.txtGL_IDLavoro.Size = New System.Drawing.Size(94, 20)
         Me.txtGL_IDLavoro.TabIndex = 19
         '
         'txtGL_AppObjUID
         '
-        Me.txtGL_AppObjUID.Location = New System.Drawing.Point(315, 72)
+        Me.txtGL_AppObjUID.Location = New System.Drawing.Point(285, 82)
         Me.txtGL_AppObjUID.Name = "txtGL_AppObjUID"
-        Me.txtGL_AppObjUID.Size = New System.Drawing.Size(87, 20)
+        Me.txtGL_AppObjUID.Size = New System.Drawing.Size(84, 20)
         Me.txtGL_AppObjUID.TabIndex = 20
         '
         'txtGL_AziendaID
         '
-        Me.txtGL_AziendaID.Location = New System.Drawing.Point(408, 72)
+        Me.txtGL_AziendaID.Location = New System.Drawing.Point(375, 82)
         Me.txtGL_AziendaID.Name = "txtGL_AziendaID"
-        Me.txtGL_AziendaID.Size = New System.Drawing.Size(87, 20)
+        Me.txtGL_AziendaID.Size = New System.Drawing.Size(91, 20)
         Me.txtGL_AziendaID.TabIndex = 21
         '
         'btnSQLServer_Connection
         '
-        Me.btnSQLServer_Connection.Location = New System.Drawing.Point(13, 144)
+        Me.btnSQLServer_Connection.Location = New System.Drawing.Point(6, 19)
         Me.btnSQLServer_Connection.Name = "btnSQLServer_Connection"
-        Me.btnSQLServer_Connection.Size = New System.Drawing.Size(230, 23)
+        Me.btnSQLServer_Connection.Size = New System.Drawing.Size(238, 23)
         Me.btnSQLServer_Connection.TabIndex = 22
-        Me.btnSQLServer_Connection.Text = "Connect to SQL Server"
+        Me.btnSQLServer_Connection.Text = "Connect to SQL Server AND get tables names"
         Me.btnSQLServer_Connection.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.btnGetTables)
+        Me.GroupBox1.Controls.Add(Me.txtGL_AziendaID)
+        Me.GroupBox1.Controls.Add(Me.btnConnect)
+        Me.GroupBox1.Controls.Add(Me.txtGL_AppObjUID)
+        Me.GroupBox1.Controls.Add(Me.btnSelect)
+        Me.GroupBox1.Controls.Add(Me.txtGL_IDLavoro)
+        Me.GroupBox1.Controls.Add(Me.btnInsert_GL_Lavori)
+        Me.GroupBox1.Controls.Add(Me.btnCloseConnection)
+        Me.GroupBox1.Controls.Add(Me.lblStato)
+        Me.GroupBox1.Controls.Add(Me.btnSelect_Classificazione)
+        Me.GroupBox1.Controls.Add(Me.btnSelectContati)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(483, 142)
+        Me.GroupBox1.TabIndex = 23
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Postgre SQL"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnSQLServerGetAllData)
+        Me.GroupBox2.Controls.Add(Me.lblStatoSQLServer)
+        Me.GroupBox2.Controls.Add(Me.btnSQLServer_Connection)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 161)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(483, 121)
+        Me.GroupBox2.TabIndex = 24
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "SQL Server"
+        '
+        'btnSQLServerGetAllData
+        '
+        Me.btnSQLServerGetAllData.Location = New System.Drawing.Point(6, 48)
+        Me.btnSQLServerGetAllData.Name = "btnSQLServerGetAllData"
+        Me.btnSQLServerGetAllData.Size = New System.Drawing.Size(205, 23)
+        Me.btnSQLServerGetAllData.TabIndex = 23
+        Me.btnSQLServerGetAllData.Text = "Extract data from every tables"
+        Me.btnSQLServerGetAllData.UseVisualStyleBackColor = True
+        '
+        'lblStatoSQLServer
+        '
+        Me.lblStatoSQLServer.AutoSize = True
+        Me.lblStatoSQLServer.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.lblStatoSQLServer.Location = New System.Drawing.Point(250, 24)
+        Me.lblStatoSQLServer.Name = "lblStatoSQLServer"
+        Me.lblStatoSQLServer.Size = New System.Drawing.Size(107, 13)
+        Me.lblStatoSQLServer.TabIndex = 22
+        Me.lblStatoSQLServer.Text = "Stato: Non connesso"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(507, 495)
-        Me.Controls.Add(Me.btnSQLServer_Connection)
-        Me.Controls.Add(Me.txtGL_AziendaID)
-        Me.Controls.Add(Me.txtGL_AppObjUID)
-        Me.Controls.Add(Me.txtGL_IDLavoro)
-        Me.Controls.Add(Me.btnCloseConnection)
-        Me.Controls.Add(Me.btnSelect_Classificazione)
-        Me.Controls.Add(Me.btnGetTables)
         Me.Controls.Add(Me.dgvData)
-        Me.Controls.Add(Me.btnSelectContati)
-        Me.Controls.Add(Me.lblStato)
-        Me.Controls.Add(Me.btnInsert_GL_Lavori)
-        Me.Controls.Add(Me.btnSelect)
-        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -188,4 +239,8 @@ Partial Class Form1
     Friend WithEvents txtGL_AppObjUID As TextBox
     Friend WithEvents txtGL_AziendaID As TextBox
     Friend WithEvents btnSQLServer_Connection As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblStatoSQLServer As Label
+    Friend WithEvents btnSQLServerGetAllData As Button
 End Class
--- VBPostgreSQL_Connection/Form1.vb
+++ VBPostgreSQL_Connection/Form1.vb
@@ -1,26 +1,31 @@
 ﻿Imports Npgsql
+Imports NpgsqlTypes
 Imports System.Data.SqlClient
 
 
 
 Public Class Form1
-    ' COMMENTO ......
     ' Variables for manage the posdtgre db
     Public Shared connectionString As String = "Server=localhost;Port=5432;UserId=postgres;Password=admin;Database=Agile_Business;CommandTimeout=300;"
     Public Shared DBConnection As NpgsqlConnection
 
     ' SQL Server connection
     Dim SQLConnection As New SqlClient.SqlConnection
-    Dim SQLConnectionString As String = "Data Source=(localdb)\MSSQLLocalDB;initial catalog=TWPROJECT;Integrated Security=true;Connection Timeout=10;"
+    Dim SQLConnectionString As String = "Data Source=(localdb)\MSSQLLocalDB;initial catalog=AgileBusiness_VUOTO;Integrated Security=true;Connection Timeout=10;"
 
-    ' array 
+
+    ' array for PostgreSQl tables
     Public Shared dsTabelle As New DataSet
 
+    ' array for SQL Server tables
+    Public Shared dtSQLServer As New DataTable
+    ' Where I save the value of all the tables from the SQL Server DB
+    Public Shared listOfDS As New List(Of DataTable)
 
+    ' Function that execute the specified query for the Postgre DB
     Public Sub Execute_Select_Query(query As String)
 
         Dim dt As New DataTable()
-
 
         Try
             Dim command As NpgsqlCommand = DBConnection.CreateCommand()
@@ -30,17 +35,43 @@ Public Class Form1
             da.Fill(dt)
 
             dgvData.DataSource = dt
-            'dgvData.DataBind()
-
 
         Catch ex As Exception
             MsgBox("Errore: " & ex.Message, MsgBoxStyle.MsgBoxSetForeground)
-        Finally
-            DBConnection.Close()
+            'Finally
+            'DBConnection.Close()
         End Try
 
     End Sub
 
+    ' Given the name of a table, get every stored record SQL Server
+    Public Sub Get_All_Data_From_Table(tableName As String, index As Int32)
+
+        Dim dt As New DataTable()
+        Dim query = "SELECT * FROM " & tableName & ";"
+
+        Try
+            Dim command As SqlCommand = SQLConnection.CreateCommand()
+            command.CommandType = CommandType.Text
+            command.CommandText = query
+            Dim da As New SqlDataAdapter(command)
+            da.Fill(dt)
+
+            listOfDS.Add(dt)
+
+            'Console.WriteLine
+
+        Catch ex As Exception
+            MsgBox("Errore: " & ex.Message, MsgBoxStyle.MsgBoxSetForeground)
+
+        End Try
+
+        ' Finally
+        ' SQLConnection.Close()
+
+    End Sub
+
+    ' Connection with the Postgre Server
     Public Sub Database_Connection()
         Try
             DBConnection = New NpgsqlConnection(connectionString)
@@ -60,35 +91,36 @@ Public Class Form1
 
     Public Sub SQLServer_DBConnection()
 
-        Dim strCommand As String = "SELECT * FROM Tasks"
+        ' mi restituisce la lista di tutte le tabelle, ordinate alfabeticamente in base al nome
+        Dim strCommand As String = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' ORDER BY TABLE_NAME ASC;"
         Dim command As SqlCommand
         Dim da As SqlDataAdapter
-        Dim dt As New DataTable
+
         Try
             SQLConnection.ConnectionString = SQLConnectionString
             command = New SqlCommand(strCommand, SQLConnection)
             command.CommandTimeout = 3000
 
             da = New SqlDataAdapter(command)
-            da.Fill(dt)
+            da.Fill(dtSQLServer)
 
-            dgvData.DataSource = dt
+            dgvData.DataSource = dtSQLServer
 
-            MsgBox(dt.Rows.Count.ToString(), MsgBoxStyle.MsgBoxSetForeground)
+            lblStatoSQLServer.Text = "Tabelle trovate: " & dtSQLServer.Rows.Count.ToString()
 
         Catch ex As Exception
             MsgBox("Errore: " & ex.Message, MsgBoxStyle.MsgBoxSetForeground)
-        Finally
-            If SQLConnection.State = ConnectionState.Open Then
-                SQLConnection.Close()
-            End If
+            'Finally
+            'If SQLConnection.State = ConnectionState.Open Then
+            'SQLConnection.Close()
+            'End If
         End Try
 
 
 
     End Sub
 
-
+    ' DOES NOT WORK.
     Public Function Close_Connection() As Boolean
         Dim retValue = False
 
@@ -137,14 +169,29 @@ Public Class Form1
         Dim GL_AppObjUID = Convert.ToInt32(txtGL_AppObjUID.Text)
         Dim GL_AziendaID = Convert.ToInt32(txtGL_AziendaID.Text)
 
-        Dim insert_query = "insert into Public.""GL_Lavori"" (""GL_IDLavoro"", ""GL_AppObjUID"", ""GL_AziendaID"", ""GL_TipoLavoro"")" &
-            "VALUES (" & GL_IDLavoro & "," & GL_AppObjUID & "," & GL_AziendaID & ",null);"
+        Dim insert_query = "insert into Public.""GL_Lavori"" (""GL_IDLavoro"", ""GL_AppObjUID"", ""GL_AziendaID"")" &
+            "VALUES (@GL_IDLavoro, @GL_AppObjUID, @GL_AziendaID);"
 
         ' Esecuzione della query
         Try
             Dim command As NpgsqlCommand = DBConnection.CreateCommand()
-            command.CommandType = CommandType.Text
+            'command.CommandType = CommandType.Text
             command.CommandText = insert_query
+
+            ' associo i valori ai parametri della query
+            Dim par = command.Parameters.Add("@GL_IDLavoro", NpgsqlDbType.Integer)
+            par.Value = GL_IDLavoro
+
+            par = command.Parameters.Add("@GL_AppObjUID", NpgsqlDbType.Integer)
+            par.Value = GL_AppObjUID
+
+            par = command.Parameters.Add("@GL_AziendaID", NpgsqlDbType.Integer)
+            par.Value = GL_AziendaID
+
+            ' Se lo imposto come null, dà errore
+            'par = command.Parameters.Add("@GL_TipoLavoro", NpgsqlDbType.Integer)
+            'par.Value = Nothing
+
 
             ' Eseguo la insert
             Dim aff As Integer = command.ExecuteNonQuery()
@@ -153,8 +200,8 @@ Public Class Form1
 
         Catch ex As Exception
             MsgBox("Errore: " & ex.Message, MsgBoxStyle.MsgBoxSetForeground)
-        Finally
-            DBConnection.Close()
+            'Finally
+            'DBConnection.Close()
         End Try
 
 
@@ -162,7 +209,6 @@ Public Class Form1
 
     Private Sub btnGetTables_Click(sender As Object, e As EventArgs) Handles btnGetTables.Click
         Dim query_select_tables = "SELECT table_name From information_schema.tables Where table_schema ='public' And table_type ='BASE TABLE';"
-
 
         Dim dt As New DataTable()
 
@@ -178,13 +224,43 @@ Public Class Form1
             dgvData.DataSource = dt
         Catch ex As Exception
             MsgBox("Errore: " & ex.Message, MsgBoxStyle.MsgBoxSetForeground)
-        Finally
-            DBConnection.Close()
+            'Finally
+            'DBConnection.Close()
         End Try
 
     End Sub
 
+
+
+    ''' 
+    ''' SQL SERVER CODE
+    ''' 
+
     Private Sub btnSQLServer_Connection_Click(sender As Object, e As EventArgs) Handles btnSQLServer_Connection.Click
         SQLServer_DBConnection()
+    End Sub
+
+    Private Sub btnSQLServerGetAllData_Click(sender As Object, e As EventArgs) Handles btnSQLServerGetAllData.Click
+        Dim table As String
+
+        listOfDS = New List(Of DataTable)
+
+        ' scorro la lista di tutte le tabelle
+        For index = 0 To dtSQLServer.Rows.Count - 1
+
+            ' estraggo il nome della tabella nella riga "index" del datatable in cui sono salvata
+            table = dtSQLServer.Rows(index)("Table_Name").ToString()
+
+            Get_All_Data_From_Table(table, index)
+        Next
+
+        lblStatoSQLServer.Text = "Total Table Executed: " & listOfDS.Count
+
+        For index = 0 To listOfDS.Count - 1
+            If listOfDS(index).Rows.Count <> 0 Then
+                MsgBox("Table '" & listOfDS(index).TableName & "'. Pos: " & index)
+            End If
+
+        Next
     End Sub
 End Class
